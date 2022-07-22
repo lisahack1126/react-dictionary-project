558de7cd3c467d22459f6ff4525a6274e125afc3
lisahack1126 <lisa.weddington1126@gmail.com>
2022-07-22 12:44:59 -0400

    Initial commit & created header

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionary App</h1>
+      </header>
+    </div>
+  );
+}
