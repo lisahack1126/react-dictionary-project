255eee279e19bf52c19ed0bcfd6d60b9b778a7b1
lisahack1126 <lisa.weddington1126@gmail.com>
2022-07-23 15:04:20 -0400

    Rendered the meanings, definition, examples

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   /*SEARCH WHEN USER SUBMITS-inside search bar*/
@@ -30,6 +33,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
